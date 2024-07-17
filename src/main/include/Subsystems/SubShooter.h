3ad989aff55b1ad6224a58b46530345250302d23
Jared <jared.jj.jin@gmail.com>
2024-07-17 10:48:49 +1000

    Made new project

---
+++ src/main/include/Subsystems/SubShooter.h
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+#include <frc2/command/Commands.h>
+
+class SubShooter : public frc2::SubsystemBase {
+ public:
+  SubShooter();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+  frc2::CommandPtr SpinFlyWheel();
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  rev::CANSparkMax _shootermotor{1, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+};

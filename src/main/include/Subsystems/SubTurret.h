bd599f11f0c94499347c911aa7c5b59f461db3a9
Jared <jared.jj.jin@gmail.com>
2024-07-18 13:24:34 +1000

    pg 37-38, next step - adding stuff in body file

---
+++ src/main/include/Subsystems/SubTurret.h
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc2/command/Commands.h>
+#include "utilities/ICSparkMax.h"
+#include "Constants.h"
+
+class SubTurret : public frc2::SubsystemBase {
+ public:
+  SubTurret();
+  static SubTurret& GetInstance() {
+    static SubTurret inst;
+    return inst;
+  }
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  frc2::CommandPtr TurnTo{units::degree_t angle};
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  ICSparkMax _turretmotor{canid::turretmotor};
+
+  static constexpr double Gearing = 20;
+};
